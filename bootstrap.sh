347d8ae87351a1170e4dea914ea082ed1009c78f
nyo <billy31210@gmail.com>
2013-06-27 22:34:35 +0800

    add bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# CrontabController
+# 
+# author: nyogjtrc
+# date: 2013/06/27
+#
+
+echo "Welcome to CrontabController";
+
+usage_msg() {
+    echo "Usage:"
+    echo "  ./bootstrap.sh install"
+}
+
+install() {
+}
+
+case $i in
+    *)
+        usage_msg
+        exit 1
+        ;;
+esac

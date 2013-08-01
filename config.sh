fccf87f6fb33548fa32b3638de1ff631b41f366e
nyo <billy31210@gmail.com>
2013-08-01 22:24:24 +0800

    new config variable

--- config.sh
+++ config.sh
@@ -2,9 +2,13 @@
 #
 # configuration file
 
+PROJECT="Crontab Controller"
+
 # path where cron file you put in
 CRON_DATA_PATH=./cron_file
 
+# log file
+LOG_FILE=./.operation.log
 
 # tmp path
 TMP_PATH=/tmp/CrontabController
